2e1173abdd4843b81586b5334c3a718a20a90fb1
Catherine <katiiforever@gmail.com>
2021-11-29 20:58:59 -0400

    using axios and starting over

--- src/App.js
+++ src/App.js
@@ -1,61 +1,25 @@
-import React, { useState, useEffect} from 'react';
-import Weather from './Components/Weather';
-import Search from './Components/Search';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Autocomplete from '@mui/material/Autocomplete';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Weather from "./Components/Weather";
 
+const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=Caracas&units=metric&APPID=${process.env.REACT_APP_API_KEY}`;
 
-const App = () =>  {
-
-  const [data, setData] = useState([]);
-  const [query, setQuery] = useState("");
-
-
+const App = () => {
+  const [data, setData] = useState("");
 
   useEffect(() => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-    .then(res => res.json())
-    .then(
-      (result) => {
-      setData(result);
-      console.log(result);
-    },)
-  },[])
+    axios
+      .get(baseUrl)
+      .then((response) => {
+        setData(response.data);
+      });
+  }, []);
 
-  const search = (e) => {
-    if (e.key === "Enter") {
-      setQuery("");
-          }
-  };
-
-  const handleChange = (e) => 
-    e.preventDefault();
-    setInput(e.target.value.toLowerCase());
-    return (
-      <div>
-        <TextField
-          id="outlined-basic" 
-          label="Outlined"
-          variant="outlined"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment 
-               type="text"
-            placeholder="Search..."
-            onChange={(e) => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}>
-                <IconButton>
-                  <SearchIcon />
-                </IconButton>
-              </InputAdornment>
-            )
-          }} 
-        />
-        <Weather weatherData={data} />
-      </div>
-    )
+  return (
+    <div>
+      <Weather weatherData={data} />
+    </div>
+  );
 };
 
 export default App;
